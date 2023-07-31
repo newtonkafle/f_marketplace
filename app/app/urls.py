a08786d042f5f962ef31b28d3071cbb09f030c95
Newton <nkafle49@gmail.com>
2023-07-31 23:52:46 +0930

    setup for search functionality

--- app/app/urls.py
+++ app/app/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from core import views
+# from marketplace import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +11,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('vendor/', include('vendor.urls')),
     path('marketplace/', include('marketplace.urls')),
+    path('search/', views.search, name='search'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/core/views.py
+++ app/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
-from .models import Vendor
+from .models import Vendor, Product
+from django.db.models import Q
 
 
 def home(request):
@@ -9,3 +10,27 @@ def home(request):
         'vendors': vendors,
     }
     return render(request, 'home.html', context)
+
+
+def search(request):
+    address = request.GET['address']
+    latitude = request.GET['lat']
+    longitude = request.GET['long']
+    radius = request.GET['custom-radius']
+    rest_food_name = request.GET['resturant-name']
+
+    # filtering the resturant by resturant name and the food item name
+    fetch_vendor_by_product = Product.objects.filter(
+        product_title__icontains=rest_food_name, is_available=True).values_list('vendor', flat=True)
+    q1 = Q(id__in=fetch_vendor_by_product)
+    q2 = Q(vendor_name__icontains=rest_food_name,
+           is_approved=True, user__is_active=True)
+
+    vendors = Vendor.objects.filter(q1 | q2)
+
+    vendor_count = vendors.count()
+    context = {
+        'vendors': vendors,
+        'counter': vendor_count,
+    }
+    return render(request, 'marketplace/market_home.html', context)
